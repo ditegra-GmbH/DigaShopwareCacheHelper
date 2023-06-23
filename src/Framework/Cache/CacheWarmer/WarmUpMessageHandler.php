341026ffe21398eb51d1026995c4be9aa3f5e9c7
Sergej <sergej.riewe@ditegra.de>
2023-06-23 13:59:23 +0200

    add option to log which seo urls should be warmed up by cache warmer

--- src/Framework/Cache/CacheWarmer/WarmUpMessageHandler.php
+++ src/Framework/Cache/CacheWarmer/WarmUpMessageHandler.php
@@ -3,6 +3,7 @@
 namespace DigaShopwareCacheHelper\Framework\Cache\CacheWarmer;
 
 use Psr\Log\LoggerInterface;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\Routing\RouterInterface;
 use Shopware\Core\Framework\Adapter\Cache\CacheIdLoader;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
@@ -31,6 +32,8 @@ class WarmUpMessageHandler extends AbstractMessageHandler
      */
     private $cacheIdLoader;
 
+    private Connection $connection;
+
     /**
      * @internal
      */
@@ -39,12 +42,13 @@ class WarmUpMessageHandler extends AbstractMessageHandler
         SystemConfigService $systemConfigService,
         RouterInterface $router,
         CacheIdLoader $cacheIdLoader,
-
+        Connection $connection,
     ) {        
         $this->logger = $logger;
         $this->systemConfigService = $systemConfigService;
         $this->router = $router;        
-        $this->cacheIdLoader = $cacheIdLoader;        
+        $this->cacheIdLoader = $cacheIdLoader;
+        $this->connection = $connection;    
     }
 
     public static function getHandledMessages(): iterable
@@ -62,16 +66,45 @@ class WarmUpMessageHandler extends AbstractMessageHandler
             return;
         }
 
-        if ($this->cacheIdLoader->load() !== $message->getCacheId()) {
+        if ($this->cacheIdLoader->load() !== $message->getCacheId()) {            
+            $this->logger->info('Skip WarmUp because: ' . $this->cacheIdLoader->load() . ' != ' . $message->getCacheId(), [$message]);
             return;
         }
-
+        
+        $logSeoUrlsOnWarmUp = $this->systemConfigService->get('DigaShopwareCacheHelper.config.logSeoUrlsOnWarmUp');
+        
         foreach ($message->getParameters() as $parameters) {
             
-            $url = rtrim($message->getDomain(), '/') . $this->router->generate($message->getRoute(), $parameters);
+            $route = $message->getRoute();
+            $pathInfo = $this->router->generate($route, $parameters);
+            $url = rtrim($message->getDomain(), '/') . $pathInfo;
 
-            $this->logger->info('WarmUp url: ' . $url );
+            if($logSeoUrlsOnWarmUp){
+                $seoUrls[] = $this->getSeoUrls($route, $pathInfo);
+
+                $this->logger->info('WarmUp url: ' . $url, $seoUrls);
+
+            } else {
+
+                $this->logger->info('WarmUp url: ' . $url, []);                
+            }            
         }
     }
 
+    private function getSeoUrls(string $routeName, string $pathInfo) : array{
+
+        $sql = 'SELECT seo_path_info FROM seo_url 
+        WHERE `seo_url`.`route_name` =:routeName
+         AND `seo_url`.`path_info` =:pathInfo 
+         AND `seo_url`.`is_canonical` = 1
+         AND `seo_url`.`is_deleted` = 0 ';
+        
+        return $this->connection->fetchAll(
+            $sql,
+            [
+                'routeName' => $routeName,
+                'pathInfo' => $pathInfo
+            ]
+        );
+    }
 }
