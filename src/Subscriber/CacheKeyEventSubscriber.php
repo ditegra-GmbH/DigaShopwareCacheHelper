aeb255f03fcca728f434a1d4ee58681715b65532
Konstantin Tuemmler <konstantin.tuemmler@ditegra.de>
2024-04-08 15:19:38 +0200

    removed unused import

--- src/Subscriber/CacheKeyEventSubscriber.php
+++ src/Subscriber/CacheKeyEventSubscriber.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace DigaShopwareCacheHelper\Subscriber;
 
-use Shopware\Core\Framework\Util\Json;
 use DigaShopwareCacheHelper\Service\DigaLoggerFactoryService;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
