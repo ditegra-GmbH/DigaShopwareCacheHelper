7614312c1e388e1c3b9a99e065201d8259010db9
Konstantin Tuemmler <konstantin.tuemmler@ditegra.de>
2024-04-08 10:36:21 +0200

    switched CacheHelperLog to RotatingFileHandler

--- src/Service/DigaLoggerFactoryService.php
+++ src/Service/DigaLoggerFactoryService.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace DigaShopwareCacheHelper\Service;
 
-use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RotatingFileHandler;
 use Monolog\Level;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
@@ -22,17 +22,34 @@ class DigaLoggerFactoryService {
         } else {
             //instantiate new Logger instance
             $this->logger = new Logger('file_logger');
-            $this->logger->pushHandler(new StreamHandler($logFile, Level::Debug));
-        }
-    }
-    //dynamic method call
-    public function __call($name, $arguments)
-    {
-        if(method_exists($this->logger, $name)) {
-            $this->logger->{$name}($arguments[0], $arguments[1] ?? []);
+            $this->logger->pushHandler(new RotatingFileHandler($logFile, 0, Level::Debug));
         }
     }
 
+        /**
+         * dynamic method call, so we do not need to override all methods of logger
+         * 
+         * @param string $name name of called method
+         * @param array $arguments array of passed arguments to method
+         * 
+         * @see https://www.php.net/manual/de/language.oop5.overloading.php#object.call
+         */
+    public function __call($name, $arguments)
+    {
+        $message = $arguments[0]; //message string
+        $context = $arguments[1] ?? []; //context for logger method, could be null, so defaults to []
+
+        if(method_exists($this->logger, $name)) {
+            $this->logger->{$name}(
+                $message, 
+                $context
+            );
+        }
+    }
+
+    /**
+     * special case since log() needs an additional parameter: $level
+     */
     public function log($level, string|\Stringable $message, array $context = []): void {
         $this->logger->log($level, $message, $context);
     }
